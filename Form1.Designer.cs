daee62cfa6102bbfae45e52ff04a9b601e6cff47
Anas Znaidi <znaidi2003@gmail.com>
2020-11-08 17:48:56 +0100

    Fatto

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace CorsaMacchine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pbPista = new System.Windows.Forms.PictureBox();
+            this.pbCar2 = new System.Windows.Forms.PictureBox();
+            this.pbCar1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPista)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.WorkerReportsProgress = true;
+            this.backgroundWorker2.WorkerSupportsCancellation = true;
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker2.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(270, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 57);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pbPista
+            // 
+            this.pbPista.Image = global::CorsaMacchine.Properties.Resources.road_trip_background_10864211;
+            this.pbPista.Location = new System.Drawing.Point(2, 98);
+            this.pbPista.Name = "pbPista";
+            this.pbPista.Size = new System.Drawing.Size(1285, 462);
+            this.pbPista.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPista.TabIndex = 4;
+            this.pbPista.TabStop = false;
+            // 
+            // pbCar2
+            // 
+            this.pbCar2.Image = global::CorsaMacchine.Properties.Resources.macchinarossa;
+            this.pbCar2.Location = new System.Drawing.Point(-7, 402);
+            this.pbCar2.Name = "pbCar2";
+            this.pbCar2.Size = new System.Drawing.Size(166, 81);
+            this.pbCar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbCar2.TabIndex = 3;
+            this.pbCar2.TabStop = false;
+            // 
+            // pbCar1
+            // 
+            this.pbCar1.Image = global::CorsaMacchine.Properties.Resources.macchinabianca;
+            this.pbCar1.Location = new System.Drawing.Point(2, 181);
+            this.pbCar1.Name = "pbCar1";
+            this.pbCar1.Size = new System.Drawing.Size(166, 81);
+            this.pbCar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbCar1.TabIndex = 2;
+            this.pbCar1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1291, 563);
+            this.Controls.Add(this.pbCar2);
+            this.Controls.Add(this.pbCar1);
+            this.Controls.Add(this.pbPista);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbPista)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pbCar1;
+        private System.Windows.Forms.PictureBox pbCar2;
+        private System.Windows.Forms.PictureBox pbPista;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CorsaMacchine
+{
+    public partial class Form1 : Form
+    {
+        public Random rnd;
+        public Form1()
+        {
+            InitializeComponent();
+            rnd = new Random();
+        }
+
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker BGW = sender as BackgroundWorker;
+            PictureBox Car = (PictureBox)e.Argument;
+
+            while (Car.Location.X < pbPista.Width - Car.Width +pbPista.Location.X)
+            {
+                if (!BGW.CancellationPending)
+                {
+                    Thread.Sleep(rnd.Next(0, 10));
+                    int x = rnd.Next(0, 4);
+                    BGW.ReportProgress(x, Car);
+                }
+            }
+            if (!BGW.CancellationPending)
+            { e.Result = Car; }
+            backgroundWorker1.CancelAsync();
+            backgroundWorker2.CancelAsync();
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            PictureBox Car = (PictureBox)e.UserState;
+            int x = Car.Location.X + e.ProgressPercentage;
+            Car.Location = new Point(x, Car.Location.Y);
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            var Car = e.Result as PictureBox;
+            if (Car == null) { MessageBox.Show("PAREGGIO"); }
+            else if (Car.Name == pbCar1.ToString()) { MessageBox.Show("THE WINNER IS BLACK CAR"); }
+            else  { MessageBox.Show("THE WINNER IS RED CAR"); }
+        }
+
+        public void Reposition()
+        {
+            pbCar1.Location = new Point(pbPista.Location.X, pbCar1.Location.Y);
+            pbCar2.Location = new Point(pbPista.Location.X, pbCar2.Location.Y);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Reposition();
+            backgroundWorker1.RunWorkerAsync(pbCar1);
+            backgroundWorker2.RunWorkerAsync(pbCar2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
+            backgroundWorker2.CancelAsync();
+        }
+    }
+}
